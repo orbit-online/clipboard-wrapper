5e45c9f97f6dd8be1bd34d0c92da68fca435739c
Anders Ingemann <aim@orbit.online>
2024-01-30 15:50:02 +0100

    Don't silence errors when tools are missing

--- clipboard.sh
+++ clipboard.sh
@@ -3,13 +3,13 @@
 clipboard_copy() {
   if type pbcopy &>/dev/null; then
     pbcopy
-  elif [[ $XDG_SESSION_TYPE = 'wayland' ]] && type wl-copy &>/dev/null; then
+  elif [[ $XDG_SESSION_TYPE = 'wayland' ]]; then
     if [[ -n $CLIPBOARD_MIME_TYPE ]]; then
       wl-copy --type "$CLIPBOARD_MIME_TYPE"
     else
       wl-copy
     fi
-  elif [[ $XDG_SESSION_TYPE = 'x11' ]] && type xsel &>/dev/null; then
+  elif [[ $XDG_SESSION_TYPE = 'x11' ]]; then
     xsel -bi
   elif type clip.exe &>/dev/null; then
     clip.exe
@@ -22,13 +22,13 @@ clipboard_copy() {
 clipboard_paste() {
   if type pbpaste &>/dev/null; then
     pbpaste
-  elif [[ $XDG_SESSION_TYPE = 'wayland' ]] && type wl-paste &>/dev/null; then
+  elif [[ $XDG_SESSION_TYPE = 'wayland' ]]; then
     if [[ -n $CLIPBOARD_MIME_TYPE ]]; then
       wl-paste --type "$CLIPBOARD_MIME_TYPE"
     else
       wl-paste
     fi
-  elif [[ $XDG_SESSION_TYPE = 'x11' ]] && type xsel &>/dev/null; then
+  elif [[ $XDG_SESSION_TYPE = 'x11' ]]; then
     xsel -bo
   elif type powershell.exe &>/dev/null; then
     powershell.exe -c Get-Clipboard | sed -z 's/\r\n/\n/g' | sed -z 's/\n$//g'
